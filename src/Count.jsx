d5cfc3a17ca02fb0846d7d256e69a6fbd5ca51fe
Chandra Shekar Manne <chandumudiraj2000@gmail.com>
2024-03-23 15:38:52 +0530

    first commit

---
+++ src/Count.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Count = () => {
+  const [show,setShow]=useState(true)
+  const handleChange=()=>{
+setShow(!show)
+  }
+  return(
+  <div className='text-center '>
+  <button className='border border-black p-2 bg-purple-600 rounded font-bold text-white' onClick={handleChange}>{show?"hide":"show"}</button>
+  {show && <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi doloremque incidunt fuga labore eum cupiditate eveniet doloribus, nemo tempora alias quibusdam ipsum sapiente est fugit esse. Dolore voluptatibus ab quod!</div>}
+  </div>
+)}
+
+export default Count
