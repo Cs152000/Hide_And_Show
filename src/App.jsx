51496e7069638e0bd6c05dd4878475521a9b3f45
Chandra Shekar Manne <chandumudiraj2000@gmail.com>
2024-03-23 16:28:55 +0530

    Added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Count from './Count';
 
+import './App.css';
+
 function App() {
   
   return (
-   <>
+ <div className='bg-yellow-400 p-4 m-5 flex justify-center'>
  <Count/>
-   </>
+ </div>
   )
 }
 
--- src/Count.jsx
+++ src/Count.jsx
@@ -6,7 +6,7 @@ const Count = () => {
 setShow(!show)
   }
   return(
-  <div className='text-center '>
+  <div className='text-center'>
   <button className='border border-black p-2 bg-purple-600 rounded font-bold text-white' onClick={handleChange}>{show?"hide":"show"}</button>
   {show && <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi doloremque incidunt fuga labore eum cupiditate eveniet doloribus, nemo tempora alias quibusdam ipsum sapiente est fugit esse. Dolore voluptatibus ab quod!</div>}
   </div>
